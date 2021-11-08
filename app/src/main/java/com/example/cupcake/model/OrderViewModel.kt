363ca61fe8f80a62fc8f86b20062cb1c17952620
Zoe Lo <twinkle74426@gmail.com>
2021-11-08 08:19:22 +0800

    fragments builded

---
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.cupcake.model
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class OrderViewModel : ViewModel() {
+
+    private val _quantity = MutableLiveData(0)
+    val quantity: LiveData<Int> = _quantity
+
+    private val _flavor = MutableLiveData("")
+    val flavor: LiveData<String> = _flavor
+
+    private val _date = MutableLiveData("")
+    val date: LiveData<String> = _date
+
+    private var _price = MutableLiveData(0.0)
+    val price: LiveData<Double> = _price
+
+
+    fun setQuantity(numberCupcakes: Int){
+        _quantity.value = numberCupcakes
+    }
+
+    fun setFlavor(desiredFlavor:String){
+        _flavor.value = desiredFlavor
+    }
+
+    fun setData(pickupDate: String){
+        _date.value = pickupDate
+    }
+
+    fun hasNoFlavorSet():Boolean{
+        return _flavor.value.isNullOrEmpty()
+    }
+
+
+
+
+
+}
