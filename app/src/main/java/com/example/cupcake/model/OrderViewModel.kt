e7b79b18ab6d88b4c480a90db69793ca02a6fb57
ZoeMac <twinkle74426@gmail.com>
2021-11-08 11:52:31 +0800

    more textview set,simpledateFormat, price update

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -3,40 +3,87 @@ package com.example.cupcake.model
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
+
+//蛋糕價格常數
+private const val PRICE_PER_CAKE = 2.00
+//當日取多收3元
+private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
 
 class OrderViewModel : ViewModel() {
 
-    private val _quantity = MutableLiveData(0)
+    private val _quantity = MutableLiveData<Int>()
     val quantity: LiveData<Int> = _quantity
 
-    private val _flavor = MutableLiveData("")
+    private val _flavor = MutableLiveData<String>()
     val flavor: LiveData<String> = _flavor
 
-    private val _date = MutableLiveData("")
+    private val _date = MutableLiveData<String>()
     val date: LiveData<String> = _date
 
-    private var _price = MutableLiveData(0.0)
+    private var _price = MutableLiveData<Double>()
     val price: LiveData<Double> = _price
 
+    val dateOptions = getPickupOptions()
 
+    init {
+        resetOrder()
+    }
+
+
+    //設定數量，同時計算好總金額
     fun setQuantity(numberCupcakes: Int){
         _quantity.value = numberCupcakes
+        updatePrice()
     }
 
     fun setFlavor(desiredFlavor:String){
         _flavor.value = desiredFlavor
     }
 
-    fun setData(pickupDate: String){
+    fun setDate(pickupDate: String){
         _date.value = pickupDate
+        updatePrice()
     }
 
     fun hasNoFlavorSet():Boolean{
         return _flavor.value.isNullOrEmpty()
     }
 
+    //use SimpleDateFormat and Locale to create and return the list of pickup dates.
+    private fun getPickupOptions(): List<String>{
+        val options = mutableListOf<String>()
+        val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+
+        //Build up a list,from current date and the following 3 dates
+        repeat(4){
+            options.add(formatter.format(calendar.time))
+            calendar.add(Calendar.DATE, 1)
+        }
+        return options
+    }
 
 
+    //reset all MutableLiveData
+    fun resetOrder(){
+        _quantity.value = 0
+        _flavor.value = ""
+        _date.value = dateOptions[0]
+        _price.value = 0.0
+    }
+
+    //calculate total price
+    private fun updatePrice(){
+        var calculatedPrice = (quantity.value ?:0) * PRICE_PER_CAKE
+        /**  ?: 左邊不是空值的話直接回傳左邊的內容，否則回傳右邊的內容。 */
+
+        if(dateOptions[0] == _date.value){
+            calculatedPrice += PRICE_FOR_SAME_DAY_PICKUP
+        }
+        _price.value = calculatedPrice
+    }
 
 
 }
